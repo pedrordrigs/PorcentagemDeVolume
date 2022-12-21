557e22803fe611c50dec3488900b6f5567f0b57d
Pedro R <66704682+pedrordrigs@users.noreply.github.com>
2022-12-21 16:59:31 -0300

    project upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PercentageOfVolume {
+  class Program {
+    static void Main(string[] args) {
+      // Verifica se a função está retornando o resultado correto para o primeiro exemplo de teste
+      int resultado = funcaoRetornaQuantitadePOV(0.1m, 90);
+      if (resultado == 10) {
+        Console.WriteLine("Teste 1: PASSED");
+      } else {
+        Console.WriteLine("Teste 1: FAILED (retornou " + resultado + ")");
+      }
+
+      // Verifica se a função está retornando o resultado correto para o segundo exemplo de teste
+      resultado = funcaoRetornaQuantitadePOV(0.1m, 100);
+      if (resultado == 11) {
+        Console.WriteLine("Teste 2: PASSED");
+      } else {
+        Console.WriteLine("Teste 2: FAILED (retornou " + resultado + ")");
+      }
+
+      // Verifica se a função está retornando o resultado correto para o terceiro exemplo de teste
+      resultado = funcaoRetornaQuantitadePOV(0.2m, 70);
+      if (resultado == 17) {
+        Console.WriteLine("Teste 3: PASSED");
+      } else {
+        Console.WriteLine("Teste 3: FAILED (retornou " + resultado + ")");
+      }
+    }
+
+    static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado) {
+        return (int)(porcentagem * totalNegociado / (1 - porcentagem));
+    }
+  }
+}
